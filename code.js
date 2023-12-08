01dde06911a6c73d1b5fee1d942056ac840d763a
Howard Shaw <68131536+howardthegr8one@users.noreply.github.com>
2023-12-08 13:14:07 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,21 @@
-function permutationSort(a) {
-    return 0;
+function permutationSort(arr, queue = [], len = arr.length, iters = [0]) {
+	
+    let newArr = []
+
+    if (arr.length <= 1) {
+        iters[0]++
+        queue.push(arr[0])
+        return iters[0]
+    }
+    for (let i = 0; i < arr.length; i++) {
+        queue.push(arr[i])
+        newArr = arr.slice(0,i).concat(arr.slice(i+1, arr.length))
+        permutationSort(newArr, queue, len, iters)
+        if (queue.length == len) {
+            if (testSort(queue))
+                return iters[0]
+            queue.pop()
+        }
+        queue.pop()
+    }
 }
